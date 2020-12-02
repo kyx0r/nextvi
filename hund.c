f35e525a0c46a01d92468d8eaf8b69c62a4f9d7c
Kyryl Melekhin <k.melekhin@gmail.com>
2020-12-02 20:30:34 +0000

    test

--- hund.c
+++ hund.c
@@ -16,7 +16,6 @@
  *  You should have received a copy of the GNU General Public License
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-// 18 "hund.c" 2
 #ifndef _DEFAULT_SOURCE
 #define _DEFAULT_SOURCE
 #endif /* _DEFAULT_SOURCE */
