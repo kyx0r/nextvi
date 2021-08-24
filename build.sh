29a2df3604081a9061f3c985f464e473ef916f88
Kyryl Melekhin <k.melekhin@gmail.com>
2021-08-24 19:37:46 +0000

    replace makefile with posix sh

---
+++ build.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+CFLAGS="-pedantic -Wall -Wfatal-errors -std=c99 -D_POSIX_C_SOURCE=200809L $CFLAGS"
+SRCS="vi.c ex.c lbuf.c sbuf.c ren.c led.c uc.c term.c regex.c conf.c hund.c"
+OBJS="vi.o ex.o lbuf.o sbuf.o ren.o led.o uc.o term.o regex.o conf.o hund.o"
+OS="$(uname -s 2>/dev/null || echo not)"
+case ${CC:-0} in
+	0) CC="gcc";;
+	*) :
+esac
+if [ "$OS" = "FreeBSD" ]; then
+	CFLAGS="$CFLAGS -D_BSD_SOURCE"
+fi
+if [ "$OS" = "OpenBSD" ]; then
+	CFLAGS="$CFLAGS -D_BSD_SOURCE"
+fi
+
+split() {
+	set -f
+	old_ifs=$IFS
+	IFS=$2
+	set -- $1
+	printf '%s\n' "$@"
+	IFS=$old_ifs
+	set +f
+}
+
+split "$SRCS" " " | while read src; do
+	$CC -c $CFLAGS $src &
+	printf '%s\n' "$CC -c $CFLAGS $src"
+done
+$CC $CFLAGS $OBJS -o vi
+printf '%s\n' "$CC $CFLAGS $OBJS -o vi"
