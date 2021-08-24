c339343cd409ecf1b10594bf8a516d6b4b186a14
Kyryl Melekhin <k.melekhin@gmail.com>
2021-08-24 20:49:07 +0000

    improve build.sh

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 CFLAGS="-pedantic -Wall -Wfatal-errors -std=c99 -D_POSIX_C_SOURCE=200809L $CFLAGS"
 SRCS="vi.c ex.c lbuf.c sbuf.c ren.c led.c uc.c term.c regex.c conf.c hund.c"
 OBJS="vi.o ex.o lbuf.o sbuf.o ren.o led.o uc.o term.o regex.o conf.o hund.o"
-OS="$(uname -s 2>/dev/null || echo not)"
+OS="$(uname)"
 case ${CC:-0} in
 	0) CC="gcc";;
 	*) :
@@ -24,9 +24,28 @@ split() {
 	set +f
 }
 
-split "$SRCS" " " | while read src; do
-	$CC -c $CFLAGS $src &
-	printf '%s\n' "$CC -c $CFLAGS $src"
-done
-$CC $CFLAGS $OBJS -o vi
-printf '%s\n' "$CC $CFLAGS $OBJS -o vi"
+cls() {
+	rm *.o vi
+	exit 0
+}
+
+ins() {
+	cp -f vi /bin
+	exit 0
+}
+
+build() {
+	split "$SRCS" " " | while read src; do
+		$CC -c $CFLAGS $src &
+		printf '%s\n' "$CC -c $CFLAGS $src"
+	done
+	split "$OBJS" " " | while read obj; do
+		while [ ! -f "$obj" ]; do sleep 1; done
+	done
+	$CC $CFLAGS $OBJS -o vi
+	printf '%s\n' "$CC $CFLAGS $OBJS -o vi"
+	exit 0
+}
+
+"$@"
+build
