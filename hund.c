218bc91c742fec3571bd590633255c0312f8f482
Kyryl Melekhin <k.melekhin@gmail.com>
2021-09-03 19:46:50 +0000

    build: move _XOPEN_SOURCE to build

--- hund.c
+++ hund.c
@@ -16,7 +16,6 @@
  *  You should have received a copy of the GNU General Public License
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-#define _XOPEN_SOURCE 700
 #include <limits.h>
 #include <locale.h>
 #include <pwd.h>
