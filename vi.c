c1a6f839ed78dce4a4f894944ca2efcd7927ee5e
Kyryl Melekhin <k.melekhin@gmail.com>
2023-05-29 13:01:02 +0000

    vi.c: remember last prompt type for vv, vb

--- vi.c
+++ vi.c
@@ -47,6 +47,7 @@ static int vi_lnnum;	/* line numbers */
 static int vi_mod;	/* screen should be redrawn:
 			(1: whole screen, -1: whole screen not updating vi_col, 2: current line) */
 static char *vi_word = "\0eEwW";	/* line word navigation */
+static char vi_lastprompt[3] = ":";	/* last prompt type */
 static int vi_arg1, vi_arg2;		/* the first and second arguments */
 static char vi_msg[EXLEN+128];		/* current message */
 static char vi_charlast[8];		/* the last character searched via f, t, F, or T */
@@ -272,6 +273,7 @@ static char *vi_prompt(char *msg, char *insert, int *kmap)
 	syn_setft("/-");
 	s = led_prompt(msg, "", insert, kmap);
 	syn_setft(ex_filetype);
+	strncpy(vi_lastprompt, msg, sizeof(vi_lastprompt) - 1);
 	vi_mod = 1;
 	if (!s)
 		return NULL;
@@ -1117,7 +1119,7 @@ static void vi_pipe(int r1, int r2)
 {
 	char *text, *rep;
 	int kmap = 0;
-	char *cmd = vi_prompt("!", 0, &kmap);
+	char *cmd = vi_prompt("!!", 0, &kmap);
 	if (!cmd)
 		return;
 	text = lbuf_cp(xb, r1, r2 + 1);
@@ -1665,11 +1667,10 @@ void vi(int init)
 					ln = vi_prompt(":", "%s/^ {8}/\t/g", &kmap);
 					goto do_excmd;
 				case 'b':
-					term_push("\x02", 1); /* ^b */
-					goto prompt;
 				case 'v':
-					term_push("\x16", 1); /* ^v */
-					goto prompt;
+					term_push(vi_lastprompt, strlen(vi_lastprompt));
+					term_push(k == 'v' ? "\x16" : "\x02", 1); /* ^v : ^b */
+					break;
 				case '#':
 					vi_lnnum = 2;
 					vi_mod = 1;
@@ -1679,7 +1680,7 @@ void vi(int init)
 					goto do_excmd;
 				case '/':
 					cs = vi_curword(xb, xrow, xoff, vi_arg1);
-					ln = vi_prompt("(search) kwd:", cs, &kmap);
+					ln = vi_prompt("v/ xkwd:", cs, &kmap);
 					ex_krsset(ln, +1);
 					free(ln);
 					free(cs);
@@ -1727,7 +1728,6 @@ void vi(int init)
 				vi_mod = 1;
 				break;
 			case ':':
-				prompt:
 				ln = vi_prompt(":", 0, &kmap);
 				do_excmd:
 				if (ln && ln[0])
