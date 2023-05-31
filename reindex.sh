26b33aefa8ae5bbf7189bccdd08d074a3c6f4434
Kyryl Melekhin <k.melekhin@gmail.com>
2023-05-31 10:52:24 -0400

    reindex *

--- reindex.sh
+++ reindex.sh
@@ -1,9 +1,11 @@
 for p in *.patch
 do
+	echo "PATCH: $p"
 	patch < $p
 	git diff > ./tmp
-	patch -R < $p
+	patch -R < $p &>/dev/null
 	cp tmp $p
 	git add $p
+	echo "END PATCH: $p"
 done
 rm tmp *.orig
