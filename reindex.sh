9783a21c06e6f58797e431ef4909d569f4d4880b
Kyryl Melekhin <k.melekhin@gmail.com>
2024-02-18 09:03:54 -0500

    reindex.sh: better print

--- reindex.sh
+++ reindex.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 for p in *.patch
 do
-	echo "PATCH: $p"
+	printf "%s\n" "PATCH: $p"
 	patch < $p
 	git diff > ./tmp
 	patch -R < $p &>/dev/null
 	cp tmp $p
 	git add $p
-	echo "END PATCH: $p"
+	printf "\n"
 done
 rm -f tmp *.orig *.rej
