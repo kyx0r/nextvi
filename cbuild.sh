dea68a8edabea1bd1793fc65585ca2955271e609
xplshn <114888778+xplshn@users.noreply.github.com>
2024-07-23 19:38:51 -0300

    Update cbuild.sh

--- cbuild.sh
+++ cbuild.sh
@@ -8,11 +8,10 @@ fi
 trap 'cd "$cbuild_OPWD"' EXIT
 
 # Color escape sequences
-G="\033[32m" #    Green
-Y="\033[33m" #    Yellow
-R="\033[31m" #    Red
-B="\033[34m" #    Blue
-NC="\033[m"  #    Unset
+G="\033[32m" #     Green
+R="\033[31m" #     Red
+B="\033[34m" #     Blue
+NC="\033[m"  #     Unset
 
 log() {
 	# shellcheck disable=SC2059 # Using %s with ANSII escape sequences is not possible
@@ -51,69 +50,83 @@ case "$OS" in
 esac
 
 OPTIMIZE_FLAGS="-O2"
+
+build() {
+	require "${CC:=cc}"
+	log "$G" "Entering step: \"Build \"$(basename "$BASE")\" using \"$CC\""
+	run "$CC vi.c -o vi $OPTIMIZE_FLAGS $CFLAGS" || {
+		log "$R" "Failed during step: \"Build \"$(basename "$BASE")\" using \"$CC\""
+		exit 1
+	}
+}
+
+# Argument processing
 while [ $# -gt 0 ]; do
-    case "$1" in
-    "debug")
-        shift
-        log "$G" "Entering step: \"Apply debugging flags\""
-        OPTIMIZE_FLAGS="-O0 -g"
-        set -- build "$@"
-        ;;
-    "" | "build")
-    	shift
-    	require "${CC:=cc}"
-    	log "$G" "Entering step: \"Build \"$(basename "$BASE")\" using \"$CC\""
-    	run "$CC vi.c -o vi $OPTIMIZE_FLAGS $CFLAGS" || {
-    		log "$R" "Failed during step: \"Build \"$(basename "$BASE")\" using \"$CC\""
-    		exit 1
-    	} && exit 0
-    	;;
-    "pgobuild")
-    	shift
-    	pgobuild() {
-    		ccversion="$($CC --version)"
-    		case "$ccversion" in *clang*) clang=1 ;; esac
-    		if [ "$clang" = 1 ] && [ -z "$PROFDATA" ]; then
-    			if command -v llvm-profdata >/dev/null 2>&1; then
-    				PROFDATA=llvm-profdata
-    			elif xcrun -f llvm-profdata >/dev/null 2>&1; then
-    				PROFDATA="xcrun llvm-profdata"
-    			fi
-    			[ -z "$PROFDATA" ] && echo "pgobuild with clang requires llvm-profdata" && exit 1
-    		fi
-    		run "$CC vi.c -fprofile-generate=. -o vi -O2 $CFLAGS"
-    		echo "qq" | ./vi -v ./vi.c >/dev/null
-    		[ "$clang" = 1 ] && run "$PROFDATA" merge ./*.profraw -o default.profdata
-    		run "$CC vi.c -fprofile-use=. -o vi -O2 $CFLAGS"
-    		rm -f ./*.gcda ./*.profraw ./default.profdata
-    	}
-    	shift
-    	require "${CC:=cc}"
-    	log "$G" "Entering step: \"Build \"$(basename "$BASE")\" using \"$CC\" and PGO\""
-    	pgobuild || {
-    		log "$R" "Failed during step: \"Build \"$(basename "$BASE")\" using \"$CC\" and PGO\""
-    		exit 1
-    	} && exit 0
-    	;;
-    "clean")
-    	shift
-    	[ -x ./vi ] || {
-    		log "$R" "./vi does not exist!"
-    		exit 1
-    	} && run rm ./vi
-    	exit 0
-    	;;
-    "retrieve")
-    	[ -x ./vi ] || {
-    		log "$R" "./vi does not exist!"
-    		exit 1
-    	} && mv ./vi ./nextvi
-    	readlink -f ./nextvi
-    	exit 0
-    	;;
-    *)
-        echo "Usage: $0 {build|debug|pgobuild|clean}"
-        exit 1
-        ;;
-    esac
+	case "$1" in
+	"install")
+		shift
+		build && {
+			run mkdir -p "$DESTDIR$PREFIX/bin/" &&
+				run cp -f vi "$DESTDIR$PREFIX/bin/vi" &&
+				[ -x "$DESTDIR$PREFIX/bin/vi" ] && log "$G" "\"$(basename "$BASE")\" has been install to $DESTDIR$PREFIX/bin/vi" || log "$R" "Couldn't finish installation"
+		} && exit 0 || exit 1
+		;;
+	"debug")
+		shift
+		log "$G" 'Entering step: "Apply debugging flags"'
+		OPTIMIZE_FLAGS="-O0 -g"
+		set -- build "$@"
+		;;
+	"" | "build")
+		shift
+		build && exit 0 || exit 1
+		;;
+	"pgobuild")
+		shift
+		pgobuild() {
+			ccversion="$($CC --version)"
+			case "$ccversion" in *clang*) clang=1 ;; esac
+			if [ "$clang" = 1 ] && [ -z "$PROFDATA" ]; then
+				if command -v llvm-profdata >/dev/null 2>&1; then
+					PROFDATA=llvm-profdata
+				elif xcrun -f llvm-profdata >/dev/null 2>&1; then
+					PROFDATA="xcrun llvm-profdata"
+				fi
+				[ -z "$PROFDATA" ] && echo "pgobuild with clang requires llvm-profdata" && exit 1
+			fi
+			run "$CC vi.c -fprofile-generate=. -o vi -O2 $CFLAGS"
+			echo "qq" | ./vi -v ./vi.c >/dev/null
+			[ "$clang" = 1 ] && run "$PROFDATA" merge ./*.profraw -o default.profdata
+			run "$CC vi.c -fprofile-use=. -o vi -O2 $CFLAGS"
+			rm -f ./*.gcda ./*.profraw ./default.profdata
+		}
+		require "${CC:=cc}"
+		log "$G" "Entering step: \"Build \"$(basename "$BASE")\" using \"$CC\" and PGO\""
+		pgobuild || {
+			log "$R" "Failed during step: \"Build \"$(basename "$BASE")\" using \"$CC\" and PGO\""
+			exit 1
+		} && exit 0 || exit 1
+		;;
+	"clean")
+		shift
+		[ -x ./vi ] || {
+			log "$R" "./vi does not exist!"
+			exit 1
+		} && run rm ./vi
+		exit 0
+		;;
+	"retrieve")
+		shift
+		[ -x ./vi ] || {
+			log "$R" "./vi does not exist!"
+			exit 1
+		} && mv ./vi ./nextvi
+		readlink -f ./nextvi
+		exit 0
+		;;
+	*)
+		echo "Usage: $0 {install|pgobuild|build|debug|clean}"
+		exit 1
+		;;
+	esac
 done
