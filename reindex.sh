f25194b06a38f44a9713d12c115e5f492d4334b9
Kyryl Melekhin <k.melekhin@gmail.com>
2024-09-17 17:46:11 -0400

    reindex.sh: add flag 1 and 2

--- reindex.sh
+++ reindex.sh
@@ -3,11 +3,12 @@ for p in *.patch
 do
 	printf "%s\n" "PATCH: $p"
 	patch --merge=diff3 < $p
-	[ ! -z "$1" ] && ./cbuild.sh build
+	[ "$1" = "1" ] && ./cbuild.sh build
 	git diff > /tmp/tmp.patch
 	patch -R < $p &>/dev/null
 	cp /tmp/tmp.patch $p
 	git add $p
 	printf "\n"
+	[ "$1" = "2" ] && git reset --hard
 done
 rm -f tmp *.orig *.rej
