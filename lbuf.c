c6ae1d218e3b8f30ba9236cbeebf79a2cb698169
Kyryl Melekhin <k.melekhin@gmail.com>
2024-11-02 14:10:45 -0400

    lbuf.c: cleanup [{ sections

--- lbuf.c
+++ lbuf.c
@@ -458,21 +458,14 @@ int lbuf_search(struct lbuf *lb, rset *re, int dir, int *r,
 	return ln_n < 0 ? 0 : 1;
 }
 
-int lbuf_paragraphbeg(struct lbuf *lb, int dir, int *row, int *off)
+int lbuf_sectionbeg(struct lbuf *lb, int dir, int *row, int *off, int ch)
 {
-	while (*row >= 0 && *row < lbuf_len(lb) && *lbuf_get(lb, *row) == '\n')
+	if (ch == '\n')
+		while (*row >= 0 && *row < lbuf_len(lb) && *lbuf_get(lb, *row) == ch)
+			*row += dir;
+	else
 		*row += dir;
-	while (*row >= 0 && *row < lbuf_len(lb) && *lbuf_get(lb, *row) != '\n')
-		*row += dir;
-	*row = MAX(0, MIN(*row, lbuf_len(lb) - 1));
-	*off = 0;
-	return 0;
-}
-
-int lbuf_sectionbeg(struct lbuf *lb, int dir, int *row, int *off)
-{
-	*row += dir;
-	while (*row >= 0 && *row < lbuf_len(lb) && *lbuf_get(lb, *row) != '{')
+	while (*row >= 0 && *row < lbuf_len(lb) && *lbuf_get(lb, *row) != ch)
 		*row += dir;
 	*row = MAX(0, MIN(*row, lbuf_len(lb) - 1));
 	*off = 0;
