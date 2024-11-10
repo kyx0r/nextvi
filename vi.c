bd882e58865d819426cd2d80f2cb3176452ac9e6
Kyryl Melekhin <k.melekhin@gmail.com>
2024-11-10 10:06:38 -0500

    vi.c: fix linenumber edge case

--- vi.c
+++ vi.c
@@ -207,7 +207,7 @@ static void vi_drawrow(int row)
 		preserve(int, syn_blockhl, 0)
 		syn_setft("/##");
 		if ((lnnum == 1 || lnnum & 4) && xled && !xleft && vi_lncol) {
-			for (i1 = 0; strchr(" \t", *rstate->chrs[ren_off(s, i1, 1)]);)
+			for (i1 = 0; strchr(" \t", *rstate->chrs[ren_off(s, i1, 0)]);)
 				i1 = ren_next(s, i1, 1);
 			i1 -= (itoa(abs(xrow-row+vi_rshift), tmp1) - tmp1)+1;
 			if (i1 >= 0) {
