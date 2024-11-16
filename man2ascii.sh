43c7772391f8e63c1ab055850d34c1baabfb283b
Kyryl Melekhin <k.melekhin@gmail.com>
2024-11-16 12:45:56 -0500

    man2ascii.sh: automatic date

--- man2ascii.sh
+++ man2ascii.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
-EXINIT='/CODE MAP/|.+4,.+18!./gencodemap.sh|wq'
+current_date=$(date +"%b %e, %Y")
+EXINIT=$'c .Dd ${current_date}\n.\n|/CODE MAP/|.+4,.+18!./gencodemap.sh|wq'
+eval "EXINIT=\"$EXINIT\""
 vi -s ./vi.1
 man -T ascii ./vi.1|col -b > README
