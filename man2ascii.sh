07fcf66ab8b28291305f910cd71a03d6c4748885
Kyryl Melekhin <k.melekhin@gmail.com>
2024-11-21 22:03:43 -0500

    update to new separator

--- man2ascii.sh
+++ man2ascii.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 current_date=$(date +"%b %e, %Y")
-EXINIT=$'c .Dd ${current_date}\n.\n|/CODE MAP/|.+4,.+18!./gencodemap.sh|wq'
+EXINIT=$'c .Dd ${current_date}\n.\n:/CODE MAP/:.+4,.+18!./gencodemap.sh:wq'
 eval "EXINIT=\"$EXINIT\""
 vi -s ./vi.1
 man -T ascii ./vi.1|col -b > README
