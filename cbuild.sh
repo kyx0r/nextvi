caeba49681f686f2590b457264e69a07ecfcc09f
Kyryl Melekhin <k.melekhin@gmail.com>
2025-06-09 01:37:26 +0000

    cbuild.sh: more consistent pgo run

--- cbuild.sh
+++ cbuild.sh
@@ -120,7 +120,7 @@ while [ $# -gt 0 ] || [ "$1" = "" ]; do
                 [ -z "$PROFDATA" ] && log "R" "pgobuild with clang requires llvm-profdata" && exit 1
             fi
             run "$CC vi.c -fprofile-generate=. -o vi -O2 $CFLAGS"
-            export EXINIT=$'&dw100.1\\\\:/not matching:&100J0300liinsert:&ewbgwZz'
+            export EXINIT=$'&dw100.1\\\\:/not matching:&:b0:&100J0300liinsert:&ewbgwZz'
             ./vi ./vi.c > /dev/null
             [ "$clang" = 1 ] && run "$PROFDATA" merge ./*.profraw -o default.profdata
             run "$CC vi.c -fprofile-use=. -o vi -O2 $CFLAGS"
