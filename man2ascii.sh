301817fc72a22cd3793e00341be6c8cdcde13c59
Kyryl Melekhin <k.melekhin@gmail.com>
2025-06-30 02:15:46 +0000

    man2ascii.sh: shellcheck

--- man2ascii.sh
+++ man2ascii.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-current_date=$(date +"%b %e, %Y")
-export EXINIT=$'c .Dd ${current_date}\n.\n:/CODE MAP/:.+4,.+18!./gencodemap.sh:wq'
+export current_date="$(date +"%b %e, %Y")"
+export EXINIT="$(printf '%b' 'c .Dd ${current_date}\n.\n:/CODE MAP/:.+4,.+18!./gencodemap.sh:wq')"
 eval "EXINIT=\"$EXINIT\""
 vi -s ./vi.1
 man -T ascii ./vi.1 > README
-EXINIT=$'%s/.\x8(.)/\\1/g:wq' vi -s ./README
+EXINIT="$(printf '%b' '%s/.\x8(.)/\\1/g:wq')" vi -s ./README
