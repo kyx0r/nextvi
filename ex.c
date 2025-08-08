4b8db2555aead29ab86f9dcbca60288459e7fc07
Kyryl Melekhin <k.melekhin@gmail.com>
2025-08-08 00:11:02 +0000

    ex.c: fixup hrange xoff

--- ex.c
+++ ex.c
@@ -253,7 +253,7 @@ static int ex_range(char **num, int n, int *row)
 #define ex_region(loc, beg, end) ex_oregion(loc, beg, end, NULL, NULL)
 static int ex_oregion(char *loc, int *beg, int *end, int *o1, int *o2)
 {
-	int naddr = 0;
+	int vaddr = 0, haddr = 0, ooff = xoff;
 	if (!strcmp("%", loc) || !lbuf_len(xb)) {
 		*beg = 0;
 		*end = MAX(0, lbuf_len(xb));
@@ -264,14 +264,14 @@ static int ex_oregion(char *loc, int *beg, int *end, int *o1, int *o2)
 			loc++;
 			if (loc[-1] == ',')
 				goto skip;
-			if (o1 && *o2 >= 0)
-				*o1 = *o2;
-			xoff = ex_range(&loc, xoff, naddr ? beg : &xrow);
-			if (o2)
+			xoff = ex_range(&loc, ooff, vaddr ? beg : &xrow);
+			if (o2 && haddr++ % 2)
 				*o2 = xoff;
+			else if (o1)
+				*o1 = xoff;
 		} else {
 			skip:
-			if (naddr++ % 2)
+			if (vaddr++ % 2)
 				*end = ex_range(&loc, xrow, NULL) + 1;
 			else
 				*beg = ex_range(&loc, xrow, NULL);
@@ -279,14 +279,14 @@ static int ex_oregion(char *loc, int *beg, int *end, int *o1, int *o2)
 		while (*loc && *loc != ';' && *loc != ',')
 			loc++;
 	}
-	if (!naddr) {
+	if (!vaddr) {
 		*beg = xrow;
 		*end = MIN(lbuf_len(xb), xrow + 1);
 		return 0;
 	}
 	if (*beg < 0 || *beg >= lbuf_len(xb))
 		return 1;
-	if (naddr < 2)
+	if (vaddr < 2)
 		*end = *beg + 1;
 	else if (*end <= *beg || *end > lbuf_len(xb))
 		return 1;
@@ -620,19 +620,18 @@ static int ec_insert(char *loc, char *cmd, char *arg)
 	int o1 = -1, o2 = -1;
 	if (ex_oregion(loc, &beg, &end, &o1, &o2))
 		return 2;
-	char *ln = o2 >= 0 ? lbuf_get(xb, beg) : NULL;
+	char *ln = o1 >= 0 ? lbuf_get(xb, beg) : NULL;
 	sbuf_smake(sb, ln && cmd[0] == 'c' ? lbuf_s(ln)->len + xcols : xcols)
 	if (cmd[0] == 'a' && (beg + 1 <= lbuf_len(xb)))
 		beg++;
 	else if (cmd[0] == 'i')
 		end = beg;
 	else if (ln) {
-		if (o1 >= 0)
-			swap(&o1, &o2);
+		xoff = o1;
 		if (rstate->s == ln)
-			n = rstate->chrs[o2] - ln;
+			n = rstate->chrs[o1] - ln;
 		else
-			n = uc_chr(ln, o2) - ln;
+			n = uc_chr(ln, o1) - ln;
 		sb->s_n = n;
 		ps = n;
 	}
@@ -650,7 +649,7 @@ static int ec_insert(char *loc, char *cmd, char *arg)
 	if (vi_insmov != TK_CTL('c')) {
 		if (ln && cmd[0] == 'c') {
 			memcpy(sb->s, ln, n);
-			ln = o1 >= 0 ? uc_chr(ln + n, o1) : ln + n;
+			ln = o2 > o1 ? uc_chr(ln + n, o2 - o1) : ln + n;
 			sbufn_str(sb, ln)
 		} else
 			sbufn_chr(sb, xvis & 2 ? 0 : '\n')
