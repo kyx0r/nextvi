c585adb5bedb6545b548c9568c197268ea9010a8
Kyryl Melekhin <k.melekhin@gmail.com>
2025-08-14 10:21:58 +0000

    sync

--- led.c
+++ led.c
@@ -321,7 +321,7 @@ static void led_printparts(sbuf *sb, int pre, int ps,
 /* read a character from the terminal */
 char *led_read(int *kmap, int c)
 {
-	static char buf[32];
+	static char buf[5];
 	int c1, c2, i, n;
 	while (!TK_INT(c)) {
 		switch (c) {
@@ -391,6 +391,21 @@ static void led_redraw(char *cs, int r, int orow, int lsh)
 	}
 	term_pos(xrow - xtop, 0);
 	rstate--;
+}
+
+void led_modeswap()
+{
+	preserve(int, xvis, xvis & 4 ? xvis & ~4 : xvis | 4)
+	preserve(int, ftidx, ftidx)
+	if (xvis & 4)
+		ex();
+	else {
+		syn_setft(ex_ft);
+		vi(1);
+	}
+	xquit = xquit > 0 ? 0 : xquit;
+	restore(xvis)
+	restore(ftidx)
 }
 
 /* read a line from the terminal */
@@ -594,17 +609,7 @@ static void led_line(sbuf *sb, int ps, int pre, char *post, int postn,
 				led_redraw(sb->s, 0, orow, lsh);
 			continue;
 		case TK_CTL('o'):;
-			preserve(int, xvis, xvis & 4 ? xvis & ~4 : xvis | 4)
-			preserve(int, ftidx, ftidx)
-			if (xvis & 4)
-				ex();
-			else {
-				syn_setft(ex_ft);
-				vi(1);
-			}
-			xquit = xquit > 0 ? 0 : xquit;
-			restore(xvis)
-			restore(ftidx)
+			led_modeswap();
 			continue;
 		default:
 			if (c == '\n' || TK_INT(c))
