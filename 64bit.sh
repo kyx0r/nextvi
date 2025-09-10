ae84f910f32c9df79df71f3777df634ff060dc69
Kyryl Melekhin <k.melekhin@gmail.com>
2025-09-10 13:42:35 +0000

    64bit.sh: fix

--- 64bit.sh
+++ 64bit.sh
@@ -14,6 +14,7 @@ for p in *.c *.h; do
 	EXINIT="${EXINIT}:%s/s64 signo/int signo/g"
 	EXINIT="${EXINIT}:%s/s64 pipefds/int pipefds/g"
 	EXINIT="${EXINIT}:%s/s64 main\(s64/int main(int/g"
+	EXINIT="${EXINIT}:%s/waitpid\(pid, status, 0\);/waitpid(pid, (int*)status, 0);/g"
 	EXINIT="${EXINIT}:%s/INT_MAX/INT64_MAX/g"
 	EXINIT="${EXINIT}:wq" vi -sm "$p"
 done
