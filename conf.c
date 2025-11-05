eeb73dbd8b35ae8b24af7f378240daf06e7cc589
Un1q32 <joey.t.reinhart@gmail.com>
2025-11-05 18:02:49 +0000

    conf.c: highlight FILE, DIR and _t types

--- conf.c
+++ conf.c
@@ -65,9 +65,9 @@ struct highlight hls[] = {
 		A(BL | SYN_IT, BL | SYN_BS, BL | SYN_BE)},
 	{FT(c), NULL, A(RE1 | SYN_BGMK(BL1)), 0, 3},
 	{FT(c), NULL, A(RE1), 0, 1},
-	{FT(c), "\\<(?:signed|unsigned|char|short|u?int(?:64_t|32_t|16_t|8_t)?|\
+	{FT(c), "\\<(?:signed|unsigned|char|short||int|[a-z0-9_]+_t|FILE|DIR|\
 long|f(?:loat|64|32)|double|void|enum|union|typedef|static|extern|register|struct|\
-s(?:64|32|16|8)|u(?:64|32|16|8)|b32|bool|const|size_t|inline|restrict|\
+s(?:64|32|16|8)|u(?:64|32|16|8)|b32|bool|const|inline|restrict|\
 (true|false|_?_?asm_?_?|mem(?:set|cpy|cmp)|malloc|free|realloc|NULL|std(?:in|\
 out|err)|errno)|(return|for|while|if|else|do|sizeof|goto|switch|case|\
 default|break|continue))\\>", A(GR1, BL1 | SYN_BD, YE1)},
