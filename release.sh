a0ba008b4331e49197ec43115b3280d88c9502f0
Kyryl Melekhin <k.melekhin@gmail.com>
2026-01-09 00:37:56 +0000

    release.sh: improve

--- release.sh
+++ release.sh
@@ -1,7 +1,10 @@
 #!/bin/sh -e
+git checkout patches
+patches=$(ls *.patch)
+git checkout release
 git checkout master *.c *.h cbuild.sh
 git checkout manual man2ascii.sh gencodemap.sh vi.1
-git checkout patches *.patch README
+git checkout patches $patches README
 rd=$(cat README)
 ./man2ascii.sh
 printf "\n%s\n" "$rd" >> README
