48fd67266a45607a77bf23f5076586024e30d2ae
Kyryl Melekhin <k.melekhin@gmail.com>
2026-01-22 02:04:47 +0000

    ex.c: simplify :f command parsing and its ranged variants

--- vi.c
+++ vi.c
@@ -1816,7 +1816,7 @@ int main(int argc, char *argv[])
 				xvis &= ~4;
 			else {
 				fprintf(stderr, "Unknown option: -%c\n", argv[i][j]);
-				fprintf(stderr, "Nextvi-3.2 Usage: %s [-emsv] [file ...]\n", argv[0]);
+				fprintf(stderr, "Nextvi-3.3 Usage: %s [-emsv] [file ...]\n", argv[0]);
 				return EXIT_FAILURE;
 			}
 		}
