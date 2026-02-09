c99cad0ad23e7c3ce98ee6d1cca9a9d34ecff34f
Kyryl Melekhin <k.melekhin@gmail.com>
2026-02-09 12:15:19 -0100

    gitdiff2vi.sh: allow flags

--- gitdiff2vi.sh
+++ gitdiff2vi.sh
@@ -2,7 +2,7 @@
 # Convert current git diff to nextvi shell script
 # Usage: gitdiff2vi.sh [output.sh]
 
-output="${1:-tmp.sh}"
-git diff -- ":!$output" | ./patch2vi -r > "$output"
+output="${2:-tmp.sh}"
+git diff -- ":!$output" | ./patch2vi "$1" > "$output"
 chmod +x "$output"
 echo "Generated: $output"
