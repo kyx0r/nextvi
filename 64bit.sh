85430c37d324e9385fcc03a3bae656def4cf8c0f
Kyryl Melekhin <k.melekhin@gmail.com>
2026-02-13 09:43:21 -0100

    64bit.sh: misc

--- 64bit.sh
+++ 64bit.sh
@@ -27,6 +27,6 @@ for p in *.c *.h; do
 	EXINIT="${EXINIT}:%s/s64 main\(s64/int main(int/g"
 	EXINIT="${EXINIT}:%s/waitpid\(pid, status, 0\);/waitpid(pid, (int*)status, 0);/g"
 	EXINIT="${EXINIT}:%s/INT_MAX/INT64_MAX/g"
-	EXINIT="${EXINIT}:wq" vi -sm "$p"
+	EXINIT="${EXINIT}:wq" $VI -sm "$p"
 done
 EXINIT="$(printf '%b' '1:i #include <stdint.h>\ntypedef uint64_t u64;\ntypedef int64_t s64;\n.\n:wq')" $VI -sm vi.h
