fb7f89bcbf9d4ead86da236f952e1da1396b0c96
Kyryl Melekhin <k.melekhin@gmail.com>
2026-02-14 00:20:35 +0000

    vi.c: add decimal representation to char info

--- vi.c
+++ vi.c
@@ -553,8 +553,8 @@ static void vc_status(int type)
 		c = rstate->chrs[xoff];
 		uc_code(cp, c, l)
 		memcpy(cbuf, c, l);
-		snprintf(vi_msg, sizeof(vi_msg), "<%s> %08x %dL %dW S%td O%d C%d",
-			cbuf, cp, l, rstate->wid[xoff], c - lbuf_get(xb, xrow),
+		snprintf(vi_msg, sizeof(vi_msg), "<%s> 0x%x %u %dL %dW S%td O%d C%d",
+			cbuf, cp, cp, l, rstate->wid[xoff], c - lbuf_get(xb, xrow),
 			xoff, col);
 	} else {
 		snprintf(vi_msg, sizeof(vi_msg),
