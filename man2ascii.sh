d067d3faf3fa90d67bb4011e9429196aa35b28b1
Kyryl Melekhin <k.melekhin@gmail.com>
2026-04-20 16:36:36 -0100

    man2ascii.sh: use mandoc

--- man2ascii.sh
+++ man2ascii.sh
@@ -3,5 +3,5 @@ export current_date="$(date +"%b %e, %Y")"
 export EXINIT="$(printf '%b' 'c .Dd ${current_date}\n.\n:>CODE MAP>:.+4,.+18!./gencodemap.sh:wq')"
 eval "EXINIT=\"$EXINIT\""
 vi -s ./vi.1
-man -T ascii ./vi.1 > README
+mandoc -T ascii ./vi.1 > README
 EXINIT="$(printf '%b' '%s/.\x8(.)/\\1/g:wq')" vi -s ./README
