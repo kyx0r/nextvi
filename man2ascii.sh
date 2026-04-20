a15f9889d54eb19421dd022bb79d19bbd15e0a0d
Kyryl Melekhin <k.melekhin@gmail.com>
2026-04-20 16:40:40 -0100

    man2ascii.sh: change to octal esc

--- man2ascii.sh
+++ man2ascii.sh
@@ -4,4 +4,4 @@ export EXINIT="$(printf '%b' 'c .Dd ${current_date}\n.\n:>CODE MAP>:.+4,.+18!./g
 eval "EXINIT=\"$EXINIT\""
 vi -s ./vi.1
 mandoc -T ascii ./vi.1 > README
-EXINIT="$(printf '%b' '%s/.\x8(.)/\\1/g:wq')" vi -s ./README
+EXINIT="$(printf '%b' '%s/.\010(.)/\\1/g:wq')" vi -s ./README
