8c6bdb912fe0475bc73db3191d0ea79589f3ded2
Kyryl Melekhin <k.melekhin@gmail.com>
2026-06-14 14:27:28 +0000

    parity_test.sh: improve

---
+++ parity_test.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+unset EXINIT
+printf '%s\n' '? \?p\\\:1q\:p:q!' | ./vi -em vi.c
+printf '%s\n' '>\\>:reg:q!' | ./vi -em vi.c
+printf '%s\n' '>\\\>>:reg:q!' | ./vi -em vi.c
+printf '%s\n' '>\\\\>:reg:q!' | ./vi -em vi.c
+printf '%s\n' '>\\\\\>>:reg:q!' | ./vi -em vi.c
+printf '%s\n' '>\\\\\\>:reg:q!' | ./vi -em vi.c
+printf '%s\n' '? >\\\?:reg:q!' | ./vi -em vi.c
+printf '%s\n' '? >\\\\?:reg:q!' | ./vi -em vi.c
+printf '%s\n' '? >\\\\\?:reg:q!' | ./vi -em vi.c
+printf '%s\n' '? >\\\\\\?:reg:q!' | ./vi -em vi.c
+printf '%s\n' '? \?0\?\:0\?:q!' | ./vi -em vi.c
+printf '%s\n' '? \?0\\?\\:0\\?:q!' | ./vi -em vi.c
+printf '%s\n' '? \?0\\\?\\\:0\\\?:q!' | ./vi -em vi.c
+printf '%s\n' '? \?0\\\\?\\\\:0\\\\?:q!' | ./vi -em vi.c
+printf '%s\n' '? \? \\\? 0\\\\\\\?\\\\\\\:0\\\\\\\?:q!' | ./vi -em vi.c
+printf '%s\n' '? \? \\\? p \\\\\\\?' | ./vi -em vi.c
