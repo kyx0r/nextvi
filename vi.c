f454087eb5356f5fcd70b85c2824dcc96d1b0520
Kyryl Melekhin <k.melekhin@gmail.com>
2026-07-11 17:51:45 -0100

    ex.c: ec_find multiline scan only via register, loc maps it to buffer

--- vi.c
+++ vi.c
@@ -1862,7 +1862,7 @@ int main(int argc, char *argv[])
 				xvis = 0;
 			else {
 				fprintf(stderr, "Unknown option: -%c\n", argv[i][j]);
-				fprintf(stderr, "Nextvi-6.2 Usage: %s [-aemsv] [file ...]\n", argv[0]);
+				fprintf(stderr, "Nextvi-7.0 Usage: %s [-aemsv] [file ...]\n", argv[0]);
 				return EXIT_FAILURE;
 			}
 		}
