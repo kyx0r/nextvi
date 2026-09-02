1f545e48f213171ff65bbd7172e158bffa8e5f91
Kyryl Melekhin <k.melekhin@gmail.com>
2026-09-02 22:16:13 -0100

    release.sh: log commits

--- release.sh
+++ release.sh
@@ -7,6 +7,19 @@ git checkout manual -- man2ascii.sh gencodemap.sh vi.1
 git checkout patch2vi -- patch2vi.c build_patch2vi.sh test_patch2vi.sh deltas.sh
 git checkout test -- test.sh
 git checkout release -- release.sh CHANGELOG
+blurb=$(for b in master manual patches patch2vi test; do
+	printf "%-9s %s\n" "$b" "$(git rev-parse --short "$b")"
+done)
+tmp=$(mktemp)
+awk -v blk="$blurb" '
+	/^------------------$/ && prev == "" && ++n == 2 {
+		print "Source commits:"
+		print blk
+		print ""
+	}
+	{ prev = $0; print }
+' CHANGELOG > "$tmp"
+mv "$tmp" CHANGELOG
 rd=$(cat README)
 ./man2ascii.sh
 printf "\n%s\n" "$rd" >> README
