1fbeee7a4cee69242297789b452991c361783d8f
Kyryl Melekhin <k.melekhin@gmail.com>
2026-09-23 17:48:13 -0400

    ex.c: print [n] identifier when new buffer opened instead of [f]

--- conf.c
+++ conf.c
@@ -311,7 +311,7 @@ return|select|switch|type|var))\\>", A(GR1, BL1 | SYN_BD, YE1)},
 [^\\\\[^]\\^(-)(?:(\\\\(.))|[^\\\\])|\\^(-)[^\\\\]",
 		A(GR1 | SYN_BD | SYN_ATT, 1, GR1, AY1, YE, WH1, AY1, YE, WH1, AY1, YE, WH1, AY1, YE, WH1), 2},
 
-	{bar_ft, "^(\".*\").*(\\[[wrf]\\]).*$", A(AY1 | SYN_BD, BL, RE)},
+	{bar_ft, "^(\".*\").*(\\[[nwrf]\\]).*$", A(AY1 | SYN_BD, BL, RE)},
 	{bar_ft, "^<(.+)> (?:[^ ]+ )*([0-9]+L) ([0-9]+W) (S[0-9]+) (O[0-9]+) (C[0-9]+)$",
 		A(AY1 | SYN_BD, RE1, BL, YE, MA, CY1, YE1)},
 	{bar_ft, "^(\".*\").* ([0-9]{1,3}%) (L[0-9]+) (C[0-9]+) (B-?[0-9]+)?.*$",
